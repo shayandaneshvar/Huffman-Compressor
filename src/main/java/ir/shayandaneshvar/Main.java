28ddbbfbbac4168d0ac3487e6a4a284549a9d11a
Seyyed Shayan Daneshvar <daneshvarshayan1@gmail.com>
2020-02-02 02:27:25 +0330

    added Splash Screen

--- src/main/java/ir/shayandaneshvar/Main.java
+++ src/main/java/ir/shayandaneshvar/Main.java
@@ -1,18 +1,24 @@
 package ir.shayandaneshvar;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.SceneAntialiasing;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.Stack;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public final class Main extends Application {
     private AnchorPane root;
@@ -49,7 +55,7 @@ public final class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
         Scene scene = new Scene(root, root.getPrefWidth() - 20,
                 root.getPrefHeight()
                 , true, SceneAntialiasing.BALANCED);
@@ -58,5 +64,28 @@ public final class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.setTitle("Huffman Text Compressor");
         primaryStage.show();
+        if (args.length == 0) {
+            Stage stage = new Stage();
+            stage.initStyle(StageStyle.TRANSPARENT);
+            stage.initOwner(primaryStage);
+            stage.initModality(Modality.WINDOW_MODAL);
+            try {
+                URL url = new File("src/main/java/ir/shayandaneshvar/view/splash" +
+                        ".fxml").toURI().toURL();
+                AnchorPane spl = FXMLLoader.load(url);
+                Scene splash = new Scene(spl, spl.getPrefWidth(),
+                        spl.getPrefHeight(), true, SceneAntialiasing.BALANCED);
+                stage.setScene(splash);
+                new Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        Platform.runLater(() -> stage.close());
+                    }
+                }, 3200);
+                stage.show();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
